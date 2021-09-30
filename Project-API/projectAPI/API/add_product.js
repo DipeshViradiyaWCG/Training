5cfc850748cf884f8395b449a11dc0110c54e36e
DipeshViradiyaWCG <dipesh.v@webcodegenie.com>
2021-09-30 18:59:16 +0530

    API test

--- Project-API/projectAPI/API/add_product.js
+++ Project-API/projectAPI/API/add_product.js
@@ -1,6 +1,3 @@
 exports.add_product = function (req, res, next) {
-    return new Promise((resolve, reject) => {
-        resolve({msg : "Hello API user..."});
-    })
     res.status(200).json({msg : "Hello API user..."});
 };
